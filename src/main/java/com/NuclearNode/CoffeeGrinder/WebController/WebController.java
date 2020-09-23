88359a4467ac096736a9dccd1005b9627e0eb92e
unknown <btchavez@cpp.edu>
2020-09-23 11:50:34 -0700

    added JSoup method example

--- src/main/java/com/NuclearNode/CoffeeGrinder/WebController/WebController.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/WebController/WebController.java
@@ -1,8 +1,16 @@
 package com.NuclearNode.CoffeeGrinder.WebController;
+package com.mkyong
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 @RestController
 public class WebController {
@@ -12,5 +20,31 @@ public class WebController {
 	{
 		return "CoffeeGrinder Test";
 	}
+	
+}
+	
+public class JSoupExample{
+	
+	public static void main(String[] args) {
+		Document doc;
+		try {
+			
+			doc = JSoup.connect("http://google.com").get();
+			
+			String title = doc.title;
+			System.out.println("Title: " + title);
+			
+			Elements links = doc.select("a[href]");
+			for(Element link: links) {
+				System.out.println("\nlink: " + link.attr("href"));
+				System.out.println("text: " + link.text());
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
+
+
