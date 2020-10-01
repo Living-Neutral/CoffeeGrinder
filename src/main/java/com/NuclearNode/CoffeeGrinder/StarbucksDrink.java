fe1d24c50cac8469a5fb843bdaca1b48fa6bc66c
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-09-30 23:59:20 -0700

    added Starbucks drink class

---
+++ src/main/java/com/NuclearNode/CoffeeGrinder/StarbucksDrink.java
@@ -0,0 +1,26 @@
+package com.NuclearNode.CoffeeGrinder;
+
+public class StarbucksDrink 
+{
+	String name;
+	String description;
+	String type;
+	String category;
+	float sugar_content;
+	float serving_size;
+	float relative_sugar;
+	boolean dairy;
+	boolean soy;
+	boolean tree_nuts;
+	boolean espresso;
+	boolean wheat;
+	boolean sweetness;
+	boolean fruity;
+	
+	StarbucksDrink()
+	{
+		
+		
+	}
+
+}
