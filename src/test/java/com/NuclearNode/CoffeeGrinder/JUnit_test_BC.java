68797c85199ae0bfe3791297123304d8f52095a3
unknown <btchavez@cpp.edu>
2020-10-05 14:14:37 -0700

    test class for A6

--- src/test/java/com/NuclearNode/CoffeeGrinder/JUnit_test_BC.java
+++ src/test/java/com/NuclearNode/CoffeeGrinder/JUnit_test_BC.java
@@ -1,2 +1,35 @@
+package com.NuclearNode.CoffeeGrinder;
+
+import org.junit.*;
+
 public class JUnit_test_BC {
+
+    private Calculator_A6 calc;
+
+    @Before
+    public void setup(){
+        System.out.println("Before testing...");
+        calc = new Calculator_A6();
+    }
+
+    @After
+    public void cleanup(){
+        System.out.println("Finishing and cleaning up test");
+    }
+
+    @Test
+    public void testAdd() {
+        int answer = calc.add(10, 101);
+        Assert.assertEquals(111, answer);
+    }
+
+    public void testAdd2(){
+        int answer = calc.add(-5, 6);
+        Assert.assertEquals(1, answer);
+    }
+
+    public void testAdd3(){
+        int answer = calc.add(-5, 6);
+        Assert.assertEquals(1, answer);
+    }
 }
