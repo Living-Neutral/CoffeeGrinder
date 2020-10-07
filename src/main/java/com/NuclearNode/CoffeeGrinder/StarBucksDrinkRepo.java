b3e36c322492219697394946e73b37d081b24169
unknown <btchavez@cpp.edu>
2020-10-07 10:37:30 -0700

    added Repo item import

--- src/main/java/com/NuclearNode/CoffeeGrinder/StarBucksDrinkRepo.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/StarBucksDrinkRepo.java
@@ -2,6 +2,8 @@ package com.NuclearNode.CoffeeGrinder;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.NuclearNode.CoffeeGrinder.StarbucksDrink;
+
 public interface StarBucksDrinkRepo extends CrudRepository<StarbucksDrink,String>
 {
 	
