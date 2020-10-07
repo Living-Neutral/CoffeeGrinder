c970992f08964d78c99cf70d7420dd1fb121beab
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-10-07 10:44:51 -0700

    started on repo connection

--- src/main/java/com/NuclearNode/CoffeeGrinder/StarbucksDrinkService.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/StarbucksDrinkService.java
@@ -1,12 +1,20 @@
 package com.NuclearNode.CoffeeGrinder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StarbucksDrinkService 
 {
+	@Autowired
+	private StarBucksDrinkRepo sb_repo;
+	
 	public List<StarbucksDrink> getAllStarBucksDrinks()
 	{
+		List<StarbucksDrink> sb_drinks = new ArrayList <StarbucksDrink>();
 		
 	}
 	
