868f00f2e22a80066419969762be440a12d89d07
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-10-07 09:52:17 -0700

    created repo for manipulating database

---
+++ src/main/java/com/NuclearNode/CoffeeGrinder/StarBucksDrinkRepo.java
@@ -0,0 +1,9 @@
+package com.NuclearNode.CoffeeGrinder;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StarBucksDrinkRepo extends CrudRepository<StarbucksDrink,String>
+{
+	
+
+}
