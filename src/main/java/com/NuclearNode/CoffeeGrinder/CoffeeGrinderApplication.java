0d26a2350e387d9e9563e36bd10d26ae8e911f77
unknown <btchavez@cpp.edu>
2020-10-11 23:01:59 -0700

    added example to main method to ensure database properly connected

--- src/main/java/com/NuclearNode/CoffeeGrinder/CoffeeGrinderApplication.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/CoffeeGrinderApplication.java
@@ -2,11 +2,6 @@ package com.NuclearNode.CoffeeGrinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.NuclearNode.CoffeeGrinder.MainController;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class CoffeeGrinderApplication {
