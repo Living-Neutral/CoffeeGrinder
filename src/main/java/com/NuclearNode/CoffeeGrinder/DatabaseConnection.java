752e13707a7478a3b5420726c22eaecc690f0589
unknown <btchavez@cpp.edu>
2020-10-11 22:57:56 -0700

    created

---
+++ src/main/java/com/NuclearNode/CoffeeGrinder/DatabaseConnection.java
@@ -0,0 +1,46 @@
+package com.NuclearNode.CoffeeGrinder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/starbucks_drinks";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "NyanCat7!";
+
+    private Connection connection;
+
+    public Connection connect(){
+        if(connection == null){
+            try {
+                Class.forName(DATABASE_DRIVER);
+                Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+                System.out.print("Database Connected!");
+            }
+            catch(ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            catch(SQLException throwables)
+            {
+                throwables.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void Disconnect(){
+        if(connection!= null){
+            try{
+                connection.close();
+                connection = null;
+
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
