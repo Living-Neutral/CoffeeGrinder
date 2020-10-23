d4b3a1ab014fe0f082ca891c55a3c1ba9254d554
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-10-22 18:53:14 -0700

    got rid of add new order method

--- src/main/java/com/NuclearNode/CoffeeGrinder/MainController.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/MainController.java
@@ -25,35 +25,7 @@ public class MainController {
     @Autowired
     private StarbucksDrinkService starbucks_drink_service;
 
-    @PostMapping(path="/add")
-    public @ResponseBody String addNewOrder(@RequestParam String name, @RequestParam String description,
-                                            @RequestParam String type, @RequestParam float sugar_content,
-                                            @RequestParam String category, @RequestParam float serving_size,
-                                            @RequestParam float relative_sugar, @RequestParam boolean dairy,
-                                            @RequestParam boolean tree_nuts, @RequestParam boolean espresso,
-                                            @RequestParam boolean wheat, @RequestParam boolean sweetness,
-                                            @RequestParam boolean fruity, @RequestParam boolean soy) {
 
-        StarbucksDrink order = new StarbucksDrink();
-
-        order.setName(name);
-        order.setCategory(category);
-        order.setDairy(dairy);
-        order.setDescription(description);
-        order.setEspresso(espresso);
-        order.setType(type);
-        order.setSugar_content(sugar_content);
-        order.setServing_size(serving_size);
-        order.setRelative_sugar(relative_sugar);
-        order.setTree_nuts(tree_nuts);
-        order.setWheat(wheat);
-        order.setSweetness(sweetness);
-        order.setFruity(fruity);
-        order.setSoy(soy);
-        drinkRepository.save(order);
-
-        return "Saved.";
-    }
 
     @GetMapping(path="/all")
     public @ResponseBody Iterable<StarbucksDrink> getAllDrinks(){
