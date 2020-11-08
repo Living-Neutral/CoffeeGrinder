897ec31af616b3c8b03b91ce8f38ccb6c661d3fa
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-11-07 17:34:45 -0800

    added test mysql method

--- src/main/java/com/NuclearNode/CoffeeGrinder/MainController.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/MainController.java
@@ -2,12 +2,15 @@ package com.NuclearNode.CoffeeGrinder;
 
 import java.util.List;
 
+import java.sql.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sun.rowset.CachedRowSetImpl;
 
 @RestController
 @RequestMapping
@@ -16,6 +19,8 @@ public class MainController {
     
     @Autowired
     private StarbucksDrinkService starbucks_drink_service;
+    
+    
     
 	@RequestMapping(value = "/CoffeeGrinder/home",method=RequestMethod.GET)
 	public String home()
@@ -44,9 +49,30 @@ public class MainController {
     @Autowired
     FetchDrinkData fetchDrinkData;
 
-    @GetMapping(path = "getdata")
+    @GetMapping(path = "/getdata")
     List<DrinkModel> getDrinks(){
         return fetchDrinkData.findAll();
+    }
+    
+    @GetMapping(path="/sqlData")
+    String amazonSqlTest() throws SQLException
+    {
+    	String q1 = "SELECT * FROM CoffeeGrinder_drinks.starbucks_drink";
+    	Connection con = DriverManager.getConnection("jdbc:mysql://starbucks-drinks-online.ci8dkiszgiw2.us-east-2.rds.amazonaws.com:3299/CoffeeGrinder_drinks","root","CoffeeGrinder1!");
+    	Statement smt = con.createStatement();
+    	ResultSet rs = smt.executeQuery(q1);
+    	
+    	CachedRowSetImpl crs = new CachedRowSetImpl();
+        crs.populate(rs);
+        System.out.println(crs);
+        int i = 1;
+        String returned_result = "";
+        while(crs.next() && i<=16)
+        {
+        	returned_result+=crs.getString(i);
+        	i++;
+        }
+        return returned_result;
     }
 
 }
