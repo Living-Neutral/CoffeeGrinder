6ca209e48d1c9268bbb2ea27518989acfdbcd82d
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-11-11 12:14:54 -0800

    quick fixes

--- src/main/java/com/NuclearNode/CoffeeGrinder/QueryHandler.java
+++ src/main/java/com/NuclearNode/CoffeeGrinder/QueryHandler.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,18 @@ public class QueryHandler
 {
 	String database_name;
 	CachedRowSetImpl crs ;
-	
+	Connection con;
 	
 	QueryHandler()
 	{
 		try 
 		{
-			String q1 = "SELECT * FROM CoffeeGrinder_drinks.starbucks_drink";
-			Connection con = DriverManager.getConnection("jdbc:mysql://starbucks-drinks-online.ci8dkiszgiw2.us-east-2.rds.amazonaws.com:3299/CoffeeGrinder_drinks",
+			String q1 = "SELECT * FROM CoffeeGrinder_drinks.starbucks_drink WHERE type =\'Coffee\' AND dairy = 0";
+			con = DriverManager.getConnection("jdbc:mysql://starbucks-drinks-online.ci8dkiszgiw2.us-east-2.rds.amazonaws.com:3299/CoffeeGrinder_drinks",
 		            "root", "CoffeeGrinder1!");
 		    Statement smt = con.createStatement();
 		    ResultSet rs = smt.executeQuery(q1);
-		    CachedRowSetImpl crs = new CachedRowSetImpl();
+		    crs = new CachedRowSetImpl();
 		    crs.populate(rs);
 		}
 		
@@ -36,18 +38,106 @@ public class QueryHandler
 		}
 	}
 	
-	String returnDrinkInfo(String id)
+	void addQuery()
+	{
+		// adding to the query
+	}
+	
+	void queryTest()
+	{
+		String q1 = "SELECT * FROM CoffeeGrinder_drinks.starbucks_drink WHERE ";
+		try {
+			Statement smt2 = con.createStatement();
+			ResultSet rs = smt2.executeQuery(q1);
+			crs.populate(rs);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+
+	List <StarbucksDrink> makeSBOrder()
+	{
+		List<StarbucksDrink> list_of_sb_drinks = new ArrayList<StarbucksDrink>();
+		try {
+			while(crs.next())
+			{
+				
+				StarbucksDrink sb_drink = new StarbucksDrink();
+				
+				sb_drink.setName(crs.getString(1));
+				sb_drink.setDescription(crs.getString(2));
+				sb_drink.setType(crs.getString(3));
+				sb_drink.setTemp(crs.getString(4));
+				sb_drink.setCategory(crs.getString(5));
+				sb_drink.setImage(crs.getString(6));
+				sb_drink.setSugar_content(crs.getFloat(7));
+				sb_drink.setServing_size(crs.getFloat(8));
+				sb_drink.setRelative_sugar(crs.getFloat(9));
+				sb_drink.setDairy(crs.getBoolean(10));
+				sb_drink.setSoy(crs.getBoolean(11));
+				sb_drink.setTree_nuts(crs.getBoolean(12));
+				sb_drink.setEspresso(crs.getBoolean(13));
+				sb_drink.setWheat(crs.getBoolean(14));
+				sb_drink.setSweetness(crs.getBoolean(15));
+				sb_drink.setFruity(crs.getBoolean(16));
+				list_of_sb_drinks.add(sb_drink);
+			}
+			resetCachedSet();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list_of_sb_drinks;
+	}
+	
+	
+	
+	
+	String cachedrowtest()
+	{
+		String crs_info = "";
+		
+		try 
+		{
+			while(crs.next())
+			{
+				for(int i = 1;i<17;i++)
+				{
+					crs_info+=crs.getString(i);
+					
+				}
+				crs_info+="\n";
+				crs_info+="\n";
+			}
+			
+			resetCachedSet();
+			return crs_info;
+		}
+		
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			
+		}
+		
+		return "";
+	}
+	
+	
+	
+	String returnDrinkInfo()
 	{
 		int i = 1;
+		String drink_info = "";
 		try {
-			while(crs.next() & id != crs.getString(i))
+			while(crs.next() && i<=16)
 			{
-				if(id.equals(id))
-				{
-					return crs.getString(i+6);
-				}
-				i++;
+				drink_info+= crs.getString(i); 
 			}
+			
+			return drink_info;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -60,7 +150,7 @@ public class QueryHandler
 	{
 		try 
 		{
-			crs.getOriginal();
+			crs = (CachedRowSetImpl) crs.getOriginal();
 		}
 		
 		catch (Exception e)
