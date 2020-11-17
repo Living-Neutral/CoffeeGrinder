9acd90afa3c3d00d04e5b15b961f6ec11be26c72
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-11-17 13:40:10 -0800

    created queryhandler test class

---
+++ src/test/java/com/NuclearNode/CoffeeGrinder/QueryHandlerTest.java
@@ -0,0 +1,27 @@
+package com.NuclearNode.CoffeeGrinder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class QueryHandlerTest {
+
+	@BeforeEach
+	void setUp() throws Exception 
+	{
+		QueryHandler q_h= new QueryHandler();
+		
+	}
+	
+
+	@Test
+	void test() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+}
